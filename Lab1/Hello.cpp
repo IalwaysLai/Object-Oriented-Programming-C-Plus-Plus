ed64a2f1429a3ff2276cb63f55256ff6a320918e
IalwaysLai <llai@cpp.edu>
2025-08-27 22:17:23 -0700

    Create Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,11 @@
+/*This code will print Hello World and Hello World again!*/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf ("Hello World again! \n");
+    return 0;
+}
