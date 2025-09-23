016c51b54ab211340fb60de5ff52507e0bfd18e5
IalwaysLai <llai@cpp.edu>
2025-09-23 13:35:28 -0700

    Update vector.cpp

--- Lab2/vector.cpp
+++ Lab2/vector.cpp
@@ -7,9 +7,30 @@ using namespace std;
  * @param v - vector of integers
  */
 
- void printMemVec(int vector(), int size){
-    printf ("Vector - Each int is worth %lu bytes\n", sizeof(vector()));
-    for (int i = 0; i < size; i++){
-        printf("Value %i at Memory Location: %p\n", vector[i], vector + i);
+ void printMemVec(const vector<int>& vec){
+    printf ("Vector - Each int is worth %lu bytes \n", sizeof(vec[0]));
+    for (int i = 0; i < vec.size(); i++){
+        printf ("Value %i at Memory Location: %p\n", vec[i], &vec[i]);
     }
- }
+}
+ void incVecBy10(vector<int>& vec){
+    for (int i = 0; i < vec.size(); i++){
+        vec[i] += 10;
+    }
+}
+int main(){
+    const int SIZE = 5;
+    vector<int> arr(SIZE);
+    for (int i = 0; i < SIZE; i++){
+        arr[i] = 100 + i;
+    }
+    printf("Before-----------------\n");
+    printMemVec(arr);
+
+    //change the value.
+    incVecBy10(arr);
+    printf("After-----------------\n");
+    printMemVec(arr);
+
+    return 0;
+}
