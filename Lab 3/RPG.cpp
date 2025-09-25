fdce7ba82d0c04db29b692fc6e04b1b2bd96d4b3
IalwaysLai <llai@cpp.edu>
2025-09-25 12:11:08 -0700

    Update RPG.cpp

---
+++ Lab 3/RPG.cpp
@@ -0,0 +1,50 @@
+#include "RPG.h"
+using namespace std;
+
+//Constructors
+RPG::RPG() {
+    name = "NPC";
+    hits_taken = 0;
+    luck = 0.1;
+    exp = 50.0;
+    level = 1;
+}
+
+//Accessors
+string RPG::getName() const{
+    return name;
+}
+int RPG::gethitsTaken() const{
+    return hits_taken;
+}
+float RPG::getLuck() const{
+    return luck;
+}
+float RPG::getExp() const{
+    return exp;
+}
+int RPG::getLevel() const{
+    return level;
+}
+
+//Mutators
+
+/**
+* @brief sets hits_taken to new_hits
+*
+*/
+void RPG::setHitsTaken (int new_hits) {
+    hits_taken = new_hits;
+}
+
+/**
+* @brief returns whether hits_taken is less than MAX_HITS_TAKEN
+* In other words, a player is alive as long as they have not been hit MAX_HITS_TAKEN times.
+*
+* @return true : player is alive
+* @return false : player is unalive
+*/
+bool RPG::isAlive() const{
+    return hits_taken < MAX_HITS_TAKEN;
+}
+
