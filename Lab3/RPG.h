fa1274048a9a9861b031d3d52e5b7c50edc3690a
IalwaysLai <llai@cpp.edu>
2025-11-05 22:03:17 -0800

    Update RPG.h for Coding Assignment 4

--- Lab3/RPG.h
+++ Lab3/RPG.h
@@ -1,11 +1,10 @@
 //RPG.h
+#include <iostream>
 #include <string>
+#include <random>
 #ifndef RPG_H
 #define RPG_H
 using namespace std;
-const int INVENTORY_SIZE = 10;
-const float HIT_FACTOR = 0.05;
-const int MAX_HITS_TAKEN = 3;
 
 class RPG {
     public:
@@ -31,5 +30,48 @@ class RPG {
     float luck;
     float exp;
     int level;
+
+    //Coding Assignment 4 Modifications
+    void RPG::setName(string name){
+        this -> name = name;
+    }
+
+    void RPG::updateExplevel(){
+        exp += 50.0;
+        if (exp >= 100.0){
+            level += 1;
+            exp = 0.0;
+            luck +=0.1;
+        }
+    }
+
+    void RPG :: attack(RPG * opponent){
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_real_distribution<double> dis(0.0, 0.1);
+        float random_num = dis(gen);
+
+        const float HIT_FACTOR = 0.5;
+
+        bool hit = random_num > HIT_FACTOR * opponent -> getLuck();
+        if (hit){
+           int battle_result_hit = opponent -> gethitsTaken();
+           opponent -> setHitsTaken(battle_result_hit + 1);
+        }
+    }
+
+    void RPG :: printStats(){
+        bool alive = isAlive();
+        cout << "Name: " << name << "\tHits Taken: "<< hits_taken << "\tLuck: " << luck << "\tExp: " << exp << "\tLevel: " << level;
+        if (alive){
+            cout << "\tStatus: Alive";
+        }
+        else{
+            cout << "\tStatus: Dead:";
+        }
+        cout << endl;
+    }
+    RPG::~RPG(){
+    }
 };
 #endif
