53d70eba068f17d1a4b165d741960229ff2f3a14
IalwaysLai <llai@cpp.edu>
2025-11-06 11:10:16 -0800

    Update Game.cpp

--- Lab3/Game.cpp
+++ Lab3/Game.cpp
@@ -26,3 +26,56 @@ int Game :: selectPlayers(){
     return selectedIndex;
 }
 
+void Game :: endRound(RPG* winner, RPG* loser, int loserIndex){
+    (winner) -> setHitsTaken(0);
+    live_players.erase(loserIndex);
+    (winner) -> updateExplevel();
+    cout << "NPC_"<< (winner) -> getName() << " won against NPC_" << loser -> getName() << endl;
+}
+
+void Game :: battleRound(){
+    int playerIndex1 = selectPlayers();
+    int playerIndex2 = selectPlayers();
+    if (playerIndex1 == playerIndex2){
+        battleRound();
+        return;
+    }
+    RPG* player1 = players[playerIndex1];
+    RPG* player2 = players[playerIndex2];
+
+    while (player1 -> isAlive() && player2 -> isAlive()){
+        player1 -> attack(player2);
+        if (player2 -> isAlive()){
+            player2 -> attack(player1);
+        }
+    }
+    if (player1 -> isAlive()){
+        RPG* winner = player1;
+        RPG* loser = player2;
+        int loserIndex = playerIndex2;
+        endRound(winner, loser, loserIndex);
+    }
+    else{
+        RPG* winner = player2;
+        RPG* loser = player1;
+        int loserIndex = playerIndex1;
+        endRound(winner, loser, loserIndex);
+    }
+}
+
+void Game :: gameLoop(){
+    while(live_players.size() > 1){
+        battleRound();
+    }
+}
+
+void Game :: printFinalResults(){
+    for (int i = 0; i < players.size(); i++){
+        cout << "NPC_" << i << ": ";
+        players[i] -> printStats();
+    }
+}
+
+Game :: ~Game(){}
+
+
