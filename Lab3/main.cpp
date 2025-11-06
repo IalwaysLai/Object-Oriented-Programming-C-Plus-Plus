57e96ba38fab01b24e0eb7771bb4023bbae62b65
IalwaysLai <llai@cpp.edu>
2025-11-06 11:13:37 -0800

    Update main.cpp

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -1,30 +1,14 @@
 //main.cpp
 #include <iostream>
 #include "RPG.h"
+#include "Game.h"
 using namespace std;
 
 int main(){
-    //Ask professor question about different RPG constructors. If one is defined and one not, are they separate entities with the same name?
-    RPG p1 = RPG("Wizard", 0, 0.2, 60, 1);
-    RPG p2 = RPG();
-    //Why is c_str() necessary?
-    printf("%s Current Stats\n", p1.getName().c_str());
-    printf("Hits Taken: %i\t Luck: %f\t Exp: %f \t Level: %i\n", p1.gethitsTaken(), p1.getLuck(), p1.getExp(), p1.getLevel());
-
-    //PRINT the same for p2
-    printf("%s Current Stats\n", p2.getName().c_str());
-    printf("Hits Taken: %i\t Luck: %f\t Exp: %f \t Level: %i\n", p2.gethitsTaken(), p2.getLuck(), p2.getExp(), p2.getLevel());
-
-    //CALL setHitsTaken(new_hit) on either p1 and p2
-    int new_hits = 3;
-    p2.setHitsTaken(new_hits);
-
-    cout << "\nP2 hits taken " << p2.gethitsTaken() << endl;
-    //PRINT out the hits_taken
-
-    cout << "0 is dead, 1 is alive\n";
-    //CALL isAlive() on both p1 and p2
-    cout << p1.getName() << " " << p1.isAlive() << endl;
-    cout << p2.getName() << " " << p2.isAlive() << endl;
+   Game game;
+   int num_players = 10;
+   game.generatePlayers(num_players);
+   game.gameLoop();
+   game.printFinalResults();
     return 0;
 }
